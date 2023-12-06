d3a907df859544ff546f11f471fff06e1e1fa7fd
aqil2514 <muhamadaqil383@gmail.com>
2023-12-06 09:53:50 +0700

    fix unescaped entities

--- app/admin/gamelingo/evertale/characters/page.tsx
+++ app/admin/gamelingo/evertale/characters/page.tsx
@@ -19,7 +19,7 @@ export default async function Characters() {
 
   return (
     <div className="w-full p-10 ">
-      <h1 className="text-center font-playfair font-bold mb-5">Character's Name (Halaman Admin)</h1>
+      <h1 className="text-center font-playfair font-bold mb-5">Characters Name (Halaman Admin)</h1>
       <div>
         <Link href="/admin/gamelingo/evertale/characters/add" className="mx-2 px-2 py-1 rounded-lg font-roboto font-bold bg-amber-500 hover:bg-amber-200 hover:text-amber-950 ">
           Tambah
