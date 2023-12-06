0706e210a8ee4e73f33fa093cea7adf01767da41
aqil2514 <muhamadaqil383@gmail.com>
2023-12-06 15:46:25 +0700

    fix

--- app/api/bloggerapi/route.ts
+++
@@ -1,9 +0,0 @@
-export async function GET() {
-  try {
-    const res = await fetch(process.env.BLOGGER_API ?? "");
-
-    const api = process.env.BLOGGER_API;
-
-    return Response.json({ api, res });
-  } catch (error) {}
-}
