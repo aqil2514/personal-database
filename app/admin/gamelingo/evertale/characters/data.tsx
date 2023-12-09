bd54e991a7ab1a94ea48852fd9c4f8ece10ea5cc
aqil2514 <muhamadaqil383@gmail.com>
2023-12-09 09:17:57 +0700

    admin interface

--- app/admin/gamelingo/evertale/characters/data.tsx
+++ app/admin/gamelingo/evertale/characters/data.tsx
@@ -8,13 +8,20 @@ const BORDER = "border-solid hover:bg-emerald-500 border-2 border-white";
 
 export default function Data() {
   const [loading, setLoading] = useState<true | false>(false);
+  const [dataInit, setDataInit] = useState<React.ComponentState>();
+  const [dataBlog, setDataBlog] = useState<React.ComponentState>();
+  const [blogInit, setBlogInit] = useState<React.ComponentState>();
+  const [isDataBlog, setIsDataBlog] = useState<true | false>(false);
   const [characters, setCharacters] = useState<any>();
   async function getData() {
     try {
       setLoading(true);
       const { data } = await axios.get("/api/gamelingo/evertale");
 
-      setCharacters(data.chars);
+      const dataChar = data.chars.chars.map((char: any) => ({ charName: char.charStatus.charName, charId: char._id }));
+
+      setCharacters(dataChar);
+      setDataInit(dataChar);
     } catch (error) {
       console.error(error);
     } finally {
@@ -22,15 +29,77 @@ export default function Data() {
     }
   }
 
-  useEffect(() => {
-    if (!characters) {
-      getData();
+  async function bloggerApi() {
+    try {
+      setLoading(true);
+      const { data } = await axios.get("https://www.googleapis.com/blogger/v3/blogs/1645572543431077439/posts?key=AIzaSyB7KoRVBAojtfvAuRkG95N6KhnRGzvFTg4&maxResults=100&labels=Evertale Char&fetchImages=true");
+
+      const characters = data.items.map((char: any) => ({ charName: char.title, charLink: char.url }));
+
+      setDataBlog(characters);
+      setBlogInit(characters);
+    } catch (error) {
+      console.error();
+    } finally {
+      setLoading(false);
     }
-  }, []);
+  }
+
+  useEffect(() => {
+    getData();
+    bloggerApi();
+  }, [isDataBlog]);
+
+  function searchHandler(e: React.ChangeEvent<HTMLInputElement>) {
+    const keyword = e.target.value.toLowerCase();
+
+    if (!isDataBlog) {
+      if (keyword === "") {
+        setCharacters(dataInit);
+        return;
+      }
+
+      const charName = characters.filter((char: any) => char?.charName?.toLowerCase().includes(keyword));
+
+      setCharacters(charName);
+      return;
+    } else if (isDataBlog) {
+      if (keyword === "") {
+        setDataBlog(blogInit);
+        return;
+      }
+
+      const charName = characters.filter((char: any) => char?.charName?.toLowerCase().includes(keyword));
+
+      setDataBlog(charName);
+    }
+  }
 
   return loading ? (
     <p>Loading...</p>
   ) : (
+    <>
+      <div>
+        <input
+          type="text"
+          name="search"
+          className="block border-slate-200 focus-visible:border-green-200 w-full px-2 border-solid border-2 my-4"
+          id="search-charName"
+          placeholder={loading ? "Memuat data..." : "Cari berdasarkan nama karakter..."}
+          onChange={(e) => searchHandler(e)}
+          disabled={loading}
+        />
+        <button type="button" onClick={() => setIsDataBlog(!isDataBlog)}>
+          {isDataBlog ? "Lihat Database" : "Lihat Data Blog"}
+        </button>
+      </div>
+      {isDataBlog ? <DataBlog dataBlog={dataBlog} /> : <Database characters={characters} />}
+    </>
+  );
+}
+
+function DataBlog({ dataBlog }: { dataBlog: React.ComponentState }) {
+  return (
     <table className="w-full">
       <thead>
         <tr className="bg-emerald-950 text-white font-roboto font-bold py-1 border-solid border-2 border-white">
@@ -40,12 +109,39 @@ export default function Data() {
         </tr>
       </thead>
       <tbody className="bg-emerald-800 font-poppins text-white">
-        {characters?.chars?.map((char: React.ComponentState, i: number) => (
+        {dataBlog?.map((char: React.ComponentState, i: number) => (
           <tr key={`char-${i++}`} id={`char-${i++}`}>
             <td className={"text-center " + BORDER}>{i++}</td>
-            <td className={`px-2 ${BORDER}`}>{char?.charStatus?.charName}</td>
+            <td className={`px-2 ${BORDER}`}>{char?.charName}</td>
             <td className={"hover:bg-emerald-500 text-center " + BORDER}>
-              <Link href={`/admin/gamelingo/evertale/characters/${char?._id}`}>Detail</Link>
+              <a href={`${char?.charLink.replace("http", "https")}`} target="_blank">
+                Detail
+              </a>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+}
+
+function Database({ characters }: { characters: React.ComponentState }) {
+  return (
+    <table className="w-full">
+      <thead>
+        <tr className="bg-emerald-950 text-white font-roboto font-bold py-1 border-solid border-2 border-white">
+          <th className={`${BORDER}`}>#</th>
+          <th className={`${BORDER}`}>Characters Name</th>
+          <th className={`${BORDER}`}>Action</th>
+        </tr>
+      </thead>
+      <tbody className="bg-emerald-800 font-poppins text-white">
+        {characters?.map((char: React.ComponentState, i: number) => (
+          <tr key={`char-${i++}`} id={`char-${i++}`}>
+            <td className={"text-center " + BORDER}>{i++}</td>
+            <td className={`px-2 ${BORDER}`}>{char?.charName}</td>
+            <td className={"hover:bg-emerald-500 text-center " + BORDER}>
+              <Link href={`/admin/gamelingo/evertale/characters/${char?.charId}`}>Detail</Link>
             </td>
           </tr>
         ))}
