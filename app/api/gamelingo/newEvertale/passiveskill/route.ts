6bfd8f51277f34d07fe2493191418d508e37a004
aqil2514 <muhamadaqil383@gmail.com>
2023-12-28 10:03:08 +0700

    bug

--- app/api/gamelingo/newEvertale/passiveskill/route.ts
+++ app/api/gamelingo/newEvertale/passiveskill/route.ts
@@ -1,7 +1,9 @@
+import connectMongoDB from "@/lib/mongoose";
 import Passive from "@/models/PassiveSkills";
 import { NextResponse } from "next/server";
 
 export async function GET() {
+  await connectMongoDB;
   const passiveData = await Passive.find();
 
   return NextResponse.json({ passiveData }, { status: 200 });
