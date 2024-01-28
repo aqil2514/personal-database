8adfe19e79b5ba91752397ec6891e11c5c818c68
aqil2514 <muhamadaqil383@gmail.com>
2024-01-28 17:33:40 +0700

    middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,17 @@
-import { withAuth } from "next-auth/middleware";
+// import { withAuth } from "next-auth/middleware";
 
-export default withAuth(
-  function middleware(req) {
-    console.log(req.nextauth.token);
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) => token?.email === "muhamadaqil383@gmail.com",
-    },
-  }
-);
+// export default withAuth(
+//   function middleware(req) {
+//     console.log(req.nextauth.token);
+//   },
+//   {
+//     callbacks: {
+//       authorized: ({ token }) => token?.email === "muhamadaqil383@gmail.com",
+//     },
+//   }
+// );
+
+export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: ["/admin/:path*"],
