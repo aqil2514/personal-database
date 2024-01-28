c7bdeb785a7af5e4fc03f87983b9e5cd3a2ca1cc
aqil2514 <muhamadaqil383@gmail.com>
2024-01-28 16:46:50 +0700

    fix

--- src/app/admin/gamelingo/evertale/weapons/[id]/page.tsx
+++ src/app/admin/gamelingo/evertale/weapons/[id]/page.tsx
@@ -5,7 +5,7 @@ export const metadata: Metadata = {
   title: "Info Weapon | Personal Database",
 };
 
-export default function Weapon({ params }: Record<string, any>) {
+export default function Weapon({ params }: any) {
   const { id } = params;
   return (
     <div className="w-full min-h-full">
