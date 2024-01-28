4c0ff38e0277f39d9580dbd460659767db159671
aqil2514 <muhamadaqil383@gmail.com>
2024-01-28 18:09:51 +0700

    nextauth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,17 +1,10 @@
-// import { withAuth } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
 
-// export default withAuth(
-//   function middleware(req) {
-//     console.log(req.nextauth.token);
-//   },
-//   {
-//     callbacks: {
-//       authorized: ({ token }) => token?.email === "muhamadaqil383@gmail.com",
-//     },
-//   }
-// );
+export default withAuth({
+  secret: process.env.NEXTAUTH_SECRET,
+});
 
-export { default } from "next-auth/middleware";
+// export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: ["/admin/:path*"],
