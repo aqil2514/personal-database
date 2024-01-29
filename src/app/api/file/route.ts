a032979508895dfed5d9a8daf48e3d43144295f7
aqil2514 <muhamadaqil383@gmail.com>
2024-01-29 11:54:57 +0700

    cloudinary

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -2,6 +2,12 @@ import { NextRequest, NextResponse } from "next/server";
 import { file } from "@/lib/utils";
 import { v2 as cloudinary } from "cloudinary";
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key:process.env.CLOUDINARY_API_KEY,
+  api_secret:process.env.CLOUDINARY_SECRET
+})
+
 export async function POST(req: NextRequest) {
   const formData = await req.formData();
   const searchParams = req.nextUrl.searchParams;
